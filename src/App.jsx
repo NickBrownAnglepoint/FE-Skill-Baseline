202e0f95af1fa762ca4fd68108d561ae41e2397c
Nick Brown <nick.brown@anglepoint.com>
2024-07-22 12:34:16 -0600

    build: initial repo commit of react app

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+
+import axios from 'axios';
+import './App.css'
+
+import Header from './Components/Header';
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://dogapi.dog/api/v2/breeds')
+      .then(response => {
+        console.log(response.data.data);
+        
+        setLaunches(response.data.data);
+        console.log(launches);
+        
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <ul>
+          {launches.map(element => (
+            <li key={element.id}>
+              {element.attributes.name} - {element.attributes.description}
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {Section, Box} from '@radix-ui/themes';
+
+import dogIcon from './../assets/dog.png'
+
+const Header = () => {
+  return (
+    <Box
+        py="5"
+        style={{ backgroundColor: 'var(--gray-a2)',height: '38px', display:'flex', justifyContent:'start', alignItems:'center'}}
+    >
+          <img src={dogIcon} alt="" style={{ height: '35px', margin: '0 2em' }} />
+    </Box>
+  )
+}
+
+export default Header
