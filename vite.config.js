9e712d3e12afd3880477495d77a7d67454f03a1f
Nick Brown <nick.brown@anglepoint.com>
2024-07-23 08:28:39 -0600

    feat: configs testing and setup for linting and such

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ['lib'],
+      exclude: ["**/*.test.**"],
+      insertTypesEntry: true,
+    })],
 })
